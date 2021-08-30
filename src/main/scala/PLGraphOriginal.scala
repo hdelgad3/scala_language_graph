8288b7997f796ee7d9a241454c3a341ed871a004
hdelgad3 <hdelgado9@outlook.com>
2021-08-30 12:28:55 -0600

    finished hw01

--- src/main/scala/PLGraphOriginal.scala
+++ src/main/scala/PLGraphOriginal.scala
@@ -1,6 +1,7 @@
 import org.graphstream.graph.{Edge, Node}
 import org.graphstream.graph.implementations.{MultiGraph, SingleGraph, SingleNode}
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 
 /*
@@ -26,13 +27,42 @@ object PLGraphOriginal {
 
     // TODO: parse the PL_CSV_FILE to create a directed graph of PLs
     var reader = Source.fromFile(PL_CSV_FILE)
+    val arr = ArrayBuffer[String]()
+
+//    Read lines from file and then append line to bufferArray
     for(line <- reader.getLines()){
-      println(line)
+      arr.append(line)
     }
     reader.close()
 
+//    Send the line to the node creator
+    for(index <- 0 until arr.length){
+      addNode(graph, arr(index))
+//      println(arr(index))
+//      println(arr(index).getClass())
+    }
+
     // display the graph
     graph.display()
+  }
+
+
+  def addNode(aGraph: MultiGraph, str: String): Unit ={
+    var items = str.split(",")
+//    println(items(0) + " ,,,,,, " + items(1))
+    var firLang = items(0)
+    var secLang = items(1)
+    if(aGraph.getNode(s"$firLang") == null){
+      var node1: Node = aGraph.addNode(s"$firLang")
+      node1.addAttribute("ui.label", s"$firLang")
+    }
+    if(aGraph.getNode(s"$secLang") == null){
+      var node2: Node = aGraph.addNode(s"$secLang")
+      node2.addAttribute("ui.label", s"$secLang")
+    }
+
+    var edge: Edge = aGraph.addEdge(s"$firLang$secLang", s"$firLang", s"$secLang", true)
+
   }
 }
 
